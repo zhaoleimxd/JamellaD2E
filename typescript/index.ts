7b8dd884d2ea75504ef1098a417579de90f82c73
zhaoleimxd <zhaoleimxd@163.com>
2021-10-09 01:38:15 +0800

    Fix resource files from v3.0 exe.

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,4 +1,5 @@
 import * as fs from 'fs';
+import * as path from 'path';
 
 function splitSrcTxt() {
     let dir: string = `E:\\Work\\cpp\\JamellaD2E\\docs`;
@@ -67,6 +68,64 @@ function copyMissingFiles() {
         else {
             fs.copyFileSync("000.bmp", filePath)
         }
+    }
+}
+
+function copyMissingFiles2() {
+    let srcDir: string = `res/D2S`;
+    let outDir: string = `res/Result`;
+
+    let srcRcPath: string = `${srcDir}/1.rc`;
+
+    let jamellaSrcDir: string = `E:/Work/cpp/JamellaD2E/vs2019/JamellaD2E`;
+    let jamellaResID: string = fs.readFileSync(`${jamellaSrcDir}/ResourceIDs.h`, "UTF-8");
+    let jamellaRc: string = fs.readFileSync(`${jamellaSrcDir}/JamellaD2E.rc`, "UTF-8");
+
+    let srcRc: string = fs.readFileSync(srcRcPath, "UTF-8");
+
+    let pattern: RegExp = /(\d+)\s\S+\s"([^"]+)"/g;
+    let matches: IterableIterator<RegExpMatchArray> = srcRc.matchAll(pattern);
+    let match: IteratorResult<RegExpMatchArray> = matches.next();
+
+    let resources: Array<{
+        id: string;
+        fileName: string;
+        jamellaDefinedId: string;
+        jamellaPath: string;
+    }> = [];
+
+    while (match.done == false) {
+        let id: string = match.value[1];
+        let fileName: string = match.value[2];
+
+        let patternId: RegExp = new RegExp(`#define\\s(\\S+)\\s${id}`);
+        let ea: RegExpExecArray = patternId.exec(jamellaResID);
+
+        let jamellaDefinedId: string = ea[1];
+
+        let patternPath: RegExp = new RegExp(`${jamellaDefinedId}\\s\\S+\\s\\S+\\s"([^"]+)"`);
+        ea = patternPath.exec(jamellaRc);
+        
+        resources.push({
+            id: id,
+            fileName: fileName,
+            jamellaDefinedId: jamellaDefinedId,
+            jamellaPath: ea[1]
+        });
+
+        match = matches.next();
+    }
+
+    for (let i = 0; i < resources.length; i++) {
+        let srcPath: string = `${srcDir}/${resources[i].fileName}`;
+        let destPath: string = path.resolve(`${outDir}/${resources[i].jamellaPath}`);
+        let destPathDir: string = path.dirname(destPath);
+        
+        fs.mkdirSync(destPathDir, {
+            recursive: true
+        });
+
+        fs.copyFileSync(srcPath, destPath);
     }
 }
 
